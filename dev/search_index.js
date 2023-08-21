c17aa9def7838b03cebd016779e4151620bdb32a
Documenter.jl <documenter@juliadocs.github.io>
2023-08-21 04:16:09 +0000

    build based on 8a8b37a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EventSpaceAlgebra","category":"page"},{"location":"#EventSpaceAlgebra","page":"Home","title":"EventSpaceAlgebra","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EventSpaceAlgebra.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EventSpaceAlgebra]","category":"page"}]
+}
