631e579008c852ee9c0d5b23165e5cc0e74e8a67
okatsn <okatsn@gmail.com>
2024-10-04 09:01:47 +0000

    More strict interface for `uconvert`.

--- src/uconvert.jl
+++ src/uconvert.jl
@@ -1,9 +1,6 @@
 Unitful.uconvert(u::Unitful.FreeUnits, evtc::Depth) = Depth(uconvert(u, evtc.value))
 
 
-Unitful.uconvert(::typeof(ms_epoch), evtc::TemporalCoordinate{T,U}) where {T} where {U} = EventTimeMS{T}(evtc)
+Unitful.uconvert(::typeof(ms_epoch), evtc::TemporalCoordinate, T::DataType) = EventTimeMS{T}(evtc)
 
-function Unitful.uconvert(::typeof(jd), evtc::TemporalCoordinate{T,U}) where {T} where {U}
-    EventTimeJD{T}(evtc)
-    # print(T)
-end
+Unitful.uconvert(::typeof(jd), evtc::TemporalCoordinate, T::DataType) = EventTimeJD{T}(evtc)
