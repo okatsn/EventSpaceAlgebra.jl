74f81b7e86f99088abaad84145087c5c4e4d24ae
okatsn <okatsn@gmail.com>
2024-10-08 04:23:34 +0000

    Test for inexact julian day calculation.

--- test/datetimeconv.jl
+++ test/datetimeconv.jl
@@ -1,5 +1,5 @@
 @testset "datetimeconv.jl" begin
-    using Dates
+    using Dates, Unitful
     dt = DateTime(2023, 1, 2)
     # TODO: Test EventTime and its conversion function
     # evt = EventTime(datetime2julian(dt), JulianDay)
@@ -14,4 +14,13 @@
     @test to_datetime(evt2) == to_datetime(evt2a) == dt
     @test uconvert(u"jd", evt1a) == evt2a
     @test uconvert(u"ms_epoch", evt2a) == evt1a
+
+
+    # Test whether `epoch_julian_diff_d` are calculated correctly (against `epochms0`)
+    rounded_day = floor(EventSpaceAlgebra.epoch_julian_diff_d)
+    inexact_day = EventSpaceAlgebra.epoch_julian_diff_d - rounded_day
+
+    @test EventSpaceAlgebra.julianday0 + Day(rounded_day) + Hour(24 * inexact_day) == EventSpaceAlgebra.epochms0
+    @test to_datetime(EventTimeMS(EventSpaceAlgebra.epochms0)) == to_datetime(EventTimeJD(EventSpaceAlgebra.epochms0))
+
 end
