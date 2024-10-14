772143a9f503bfd234aa0dab0792794795dbe509
okatsn <okatsn@gmail.com>
2024-10-14 02:47:31 +0000

    Function `shift!`

--- src/eventpoint.jl
+++ src/eventpoint.jl
@@ -56,6 +56,19 @@ ArbitraryPoint(time::TemporalCoordinate, lat::Latitude, lon::Longitude, depth::D
 # Interface
 
 
+function shift!(apt::ArbitraryPoint, b::Quantity{T,D,<:typeof(deg_N)}) where {T,D}
+    apt.lat = apt.lat + uconvert(u"°", b) # because 1u"°" + 1.2u"deg_N" returns Float64
+end
+
+function shift!(apt::ArbitraryPoint, b::Quantity{T,D,<:typeof(deg_E)}) where {T,D}
+    apt.lon = apt.lon + uconvert(u"°", b)
+end
+
+function shift!(apt::ArbitraryPoint, b::Quantity{T,D,<:typeof(dep_km)}) where {T,D}
+    apt.depth = apt.depth + b
+end
+
+# +/- operations between the component in the following list is intended to be incompatible, because I want these operations to be carried out under the `shift!` interface instead (for easier code maintaining and to avoid confusion).
 for op in (:+, :-), (C, U) in [(:Latitude, :deg_N), (:Longitude, :deg_E), (:Depth, :dep_km)]
     @eval function Base.$op(::$C, ::Quantity{T,D,<:typeof($U)}) where {T,D}
         throw(UnitIncompatible())
